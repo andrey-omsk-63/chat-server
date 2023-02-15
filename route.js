09a47395ef4f983bcecbb1e9e5150fcda854aa25
andrey-omsk-63 <andrey_omsk@mail.ru>
2023-02-15 21:48:19 +0600

    '15022148'

--- route.js
+++ route.js
@@ -1,19 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    //   res.setHeader("Access-Control-Allow-Origin", "*");
+router.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-    //   res.setHeader(
-    //     "Access-Control-Allow-Methods",
-    //     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    //   );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    //   res.setHeader(
-    //     "Access-Control-Allow-Headers",
-    //     "X-Requested-With,content-type"
-    //   );
-    res.send("Это только мой мир.");
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.send('Это только мой мир.');
 });
 
 module.exports = router;

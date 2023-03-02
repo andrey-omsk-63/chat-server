bb30f88fb5d658fd46f07dd10ea3a894b8b25f65
andrey-omsk-63 <andrey_omsk@mail.ru>
2023-03-02 22:44:43 +0600

    '02032244'

--- index.js
+++ index.js
@@ -1,92 +1,96 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 const app = express();
 
-const route = require("./route");
-const { addUser, findUser, getRoomUsers, removeUser } = require("./users");
+const route = require('./route');
+const { addUser, findUser, getRoomUsers, removeUser } = require('./users');
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: '*' }));
 app.use(route);
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: '*',
+    methods: ['GET', 'POST'],
   },
 });
 
-io.on("connection", (socket) => {
-  socket.on("join", ({ name, room }) => {
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }) => {
     socket.join(room);
-    console.log("join(room)", room);
+    //console.log('join(room)', room);
     const { user, isExist } = addUser({ name, room });
 
     const userMessage = isExist
       ? `${user.name}, и снова здравствуйте`
       : `Здравствуйте, ${user.name}`;
 
-    socket.emit("message", {
+    socket.emit('message', {
       data: {
-        user: { name: "ChatAdmin" },
+        user: { name: 'ChatAdmin' },
         message: userMessage,
         date: new Date(),
         to: room,
       },
     });
 
-    socket.broadcast.to(user.room).emit("message", {
+    socket.broadcast.to(user.room).emit('message', {
       data: {
-        user: { name: "ChatAdmin" },
+        user: { name: 'ChatAdmin' },
         message: `${user.name} присоеденился`,
         date: new Date(),
         to: user.room,
       },
     });
 
-    io.to(user.room).emit("room", {
+    io.to(user.room).emit('room', {
       data: { users: getRoomUsers(user.room) },
     });
   });
 
-  socket.on("sendMessage", ({ message, params, date, to }) => {
-    const user = findUser(params);
+  socket.on('sendMessage', ({ message, params, date }) => {
+    //const user = findUser(params);
+    let user = params;
+    let to = params.room;
+    console.log('sendMessage', user, message, date, to);
 
-    if (user) {
-      io.to(user.room).emit("message", { data: { user, message, date, to } });
-    }
+    io.to(user.room).emit('message', { data: { user, message, date, to } });
+    // if (user) {
+    //io.to(user.room).emit('message', { data: { user, message, date, to } });
+    //}
   });
 
-  socket.on("leftRoom", ({ params }) => {
+  socket.on('leftRoom', ({ params }) => {
     const user = removeUser(params);
 
     if (user) {
       const { room, name } = user;
 
-      io.to(room).emit("message", {
+      io.to(room).emit('message', {
         data: {
-          user: { name: "ChatAdmin" },
+          user: { name: 'ChatAdmin' },
           message: `${name} вышел`,
           date: new Date(),
           to: room,
         },
       });
 
-      io.to(room).emit("room", {
+      io.to(room).emit('room', {
         data: { users: getRoomUsers(room) },
       });
     }
   });
 
-  io.on("disconnect", () => {
-    console.log("Отключить");
+  io.on('disconnect', () => {
+    console.log('Отключить');
   });
 });
 
 server.listen(5000, () => {
-  console.log("Сервер работает");
+  console.log('Сервер работает');
 });
 //Global
